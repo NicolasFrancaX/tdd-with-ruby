9f18d55c4408c3908ea7fc6d5eaeb3d4b3f25834
Nicolas França <nicolasfranca9@gmail.com>
2014-07-29 14:54:03 -0300

    Código que aceita IV e II.

--- lib/conversor_de_numero_romano.rb
+++ lib/conversor_de_numero_romano.rb
@@ -1,15 +1,26 @@
 class ConversorDeNumeroRomano
   TABELA = {
-    'I' => 1,
-    'V' => 5,
-    'X' => 10,
-    'L' => 50,
-    'C' => 100,
-    'D' => 500,
-    'M' => 1000
+    'I'  => 1,
+    'V'  => 5,
+    'X'  => 10,
+    'L'  => 50,
+    'C'  => 100,
+    'D'  => 500,
+    'M'  => 1000
   }
 
-  def converte numero_romano
-    TABELA[numero_romano]
+  def converte numero_em_romano
+    ultimo_vizinho_da_direita = 0
+
+    numero_em_romano.split('').
+      map { |digito| TABELA[digito] }.
+      reverse.reduce(0) do |acumulador, digito|
+        multiplicador = 1
+        multiplicador = -1 if digito < ultimo_vizinho_da_direita
+
+        ultimo_vizinho_da_direita = digito
+
+        acumulador + digito * multiplicador
+      end
   end
 end
--- test/conversor_de_numero_romano_test.rb
+++ test/conversor_de_numero_romano_test.rb
@@ -19,6 +19,26 @@ class ConversorDeNumeroRomanoTest < Test::Unit::TestCase
     atribuicao 'L', 50
   end
 
+  def teste_deve_entender_dois_simbolos_como_II
+    atribuicao 'II', 2
+  end
+
+  def teste_deve_entender_dois_simbolos_como_XX
+    atribuicao 'XX', 20
+  end
+
+  def teste_deve_entender_quatro_simbolos_dois_a_dois_como_XXII
+    atribuicao 'XXII', 22
+  end
+
+  def teste_deve_entender_numeros_como_IX
+    atribuicao 'IX', 9
+  end
+
+  def teste_deve_entender_numeros_complexos_como_XXIV
+    atribuicao 'XXIV', 24
+  end
+
   private
 
   def atribuicao numero_romano, numero_arabico
